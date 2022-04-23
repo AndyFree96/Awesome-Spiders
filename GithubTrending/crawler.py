653a3cb330d8a1b24e79beeb521ea2ec8f5a5a55
andyfree96 <anthonyfree96@gmail.com>
2022-04-24 00:43:07 +0800

    修改Github Trending

--- GithubTrending/crawler.py
+++ GithubTrending/crawler.py
@@ -15,10 +15,13 @@ for item in itemList:
     projectName = re.sub(pattern, "", item.select(".h3.lh-condensed")[0].text.strip())
     projectURL = f"https://github.com/{projectName}"
     starNumber = re.sub(pattern, "", item.select("a.Link--muted")[0].text)
+    descriptionElement = item.select("p")
+    programmingLanguageElement = item.select("[itemprop='programmingLanguage']")
     infoDict = {
         "名称": projectName,
         "链接地址":  projectURL,
-        "简介": item.select("p")[0].text.strip(),
+        "简介": descriptionElement[0].text.strip() if len(descriptionElement) else "",
+        "编程语言": programmingLanguageElement[0].text if len(programmingLanguageElement) else "",
         "总Star数": starNumber,
     }
     pprint(infoDict)
